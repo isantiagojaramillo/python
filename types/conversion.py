0bab998bb8ca50330280d2fe759dd34b8e62885f
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 11:50:24 -0500

    conversion

---
+++ types/conversion.py
@@ -0,0 +1,20 @@
+"""System module."""
+
+# x = input("")
+
+# int()
+# str()
+# float()
+# bool()
+
+print(bool(""))
+print(bool("0"))
+print(bool(None))
+print(bool(" "))
+print(bool(0))
+
+
+
+
+
+
