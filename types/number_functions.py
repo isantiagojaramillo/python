08977b30d5838d5009151fe12a5446daad7407c8
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 11:32:38 -0500

    functions of numbers

---
+++ types/number_functions.py
@@ -0,0 +1,17 @@
+"""System module."""
+import math
+
+print(round(11.3))
+print(abs(-29))
+
+## A MODULE IS ANOTHER FILE THAT CAN BE BROUGTH AND IT HAS PYTHON CODE ALREADY
+
+print(math.ceil(11.3)) # IT WILL PRINT THE CLOSEST HIGHEST NUMBER
+print(math.floor(1.9999)) # IT WILL PRINT THE CLOSEST LOWEST NUMBER
+print(math.isnan(23))
+## print(math.isnan("23"))
+print(math.pow(10, 3)) # IT WILL RAISE TO THE POWER
+print(math.sqrt(9)) # IT WILL PRINT THE SQUARE ROOT
+
+
+
