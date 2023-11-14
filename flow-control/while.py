3b6e887149f5dce5b635ab4dc4e822010d344553
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 14:32:33 -0500

    while

---
+++ flow-control/while.py
@@ -0,0 +1,17 @@
+"""System module."""
+
+# number = 1
+# while number < 100:
+#     print(number)
+#     number *= 2
+
+command = ""
+
+while command.lower() !=  "logout":
+    command = input("$ ")
+    print(command)
+
+
+
+
+
