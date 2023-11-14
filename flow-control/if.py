896dea772ea0b29edb75cd961819a497460263d9
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 12:06:46 -0500

    if, elif, else

---
+++ flow-control/if.py
@@ -0,0 +1,16 @@
+"""System module."""
+
+# age = 22
+age = 15
+
+if age > 54:
+    print("CAN WATCH THE MOVIE WITH DISCOUNT")
+elif age > 17:
+    print("CAN WATCH THE MOVIE")
+else:
+    print("CAN NOT WATCH THE MOVIE")
+    print("GO SOMEWHERE ELSE")
+
+print("DONE")
+
+
