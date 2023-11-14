ca5d7b599fe6aeb4e2ab630bfc8759bd10fbb980
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 14:14:35 -0500

    comparator chains

---
+++ flow-control/comparator_chains.py
@@ -0,0 +1,14 @@
+"""System module."""
+
+age = 25
+
+# if age >= 15 and age <= 65:
+#     print("You can not enter to the pool")
+
+if  15 <= age <= 65:
+    print("You can not enter to the pool")
+
+
+
+
+
