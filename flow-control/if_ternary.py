153dadb278b43cd6ef62aed9e931e00df7aa13c9
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 13:59:47 -0500

    ternary

---
+++ flow-control/if_ternary.py
@@ -0,0 +1,17 @@
+"""System module."""
+
+age = 15
+
+# if age > 17:
+#     message = "It is old"
+# else:
+#     message = "It is young"
+
+# print(message)
+
+message = "It is old" if age > 17 else "It is young"
+
+print(message)
+
+
+
