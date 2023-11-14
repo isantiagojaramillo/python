e9f2b86bd49226d5b8858041ffc1e9a0099b36d5
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 10:35:07 -0500

    format strings

---
+++ types/format-strings.py
@@ -0,0 +1,19 @@
+"""System module."""
+
+name = "Pepito"
+lastName = "Perez"
+
+## fullName = name + " " + lastName
+## THE f OPERATOR TO GIVE BETTER FORMAT TO THE STRINGS
+fullName = f"{name} {lastName}"
+
+print(fullName)
+
+
+
+
+
+
+
+
+
