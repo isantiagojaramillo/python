7f304dbcf3a83522c98295b2b10e8d15c770d52f
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 10:29:08 -0500

    strings

---
+++ types/strings.py
@@ -0,0 +1,26 @@
+"""System module."""
+
+COURSE_NAME = "Ultimate Python"
+COURSE_DESCRIPTION = """
+    This course has all the details you need to learn
+    to find a job as a programmer
+"""
+
+print(COURSE_NAME)
+print(COURSE_DESCRIPTION)
+
+## LEN ALLOWS YOU TO FIND THE LENGTH OF A STRING
+print(len(COURSE_NAME))
+
+print(COURSE_NAME[0])
+print(COURSE_NAME[0:8])
+print(COURSE_NAME[9:])
+print(COURSE_NAME[:8])
+print(COURSE_NAME[:])
+
+
+
+
+
+
+
