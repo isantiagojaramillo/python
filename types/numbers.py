93e2b1e82138f8bc5e92e050f6c6be7c06c58ff7
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-11-14 11:18:24 -0500

    numbers

---
+++ types/numbers.py
@@ -0,0 +1,20 @@
+"""System module."""
+
+number = 2 # INTEGER
+decimal = 1.2 # FLOAT
+imaginary = 2 + 2j # 2 + 2j i MEANS THE SQUARE ROOT OF  -1.
+
+# number = number + 2
+number += 2
+print("number ", number)
+
+print(1 + 3)
+print(1 -3)
+print(1 * 3)
+print(1 /3)
+print(1 // 3)
+print(1 % 3)
+print(2 ** 3) #RAISE TO THE POWER
+
+
+
