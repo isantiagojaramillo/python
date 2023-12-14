191c35fec235eb81a24ebb42ca67b3838b95ecde
Santiago Jaramillo <sajaramillogr@cesde.net>
2023-12-14 09:36:25 -0500

    scope

---
+++ functions/scope.py
@@ -0,0 +1,14 @@
+def greeting():
+    greet = "Hello World";
+    print(greet);
+
+def greetingChachito():
+    greet = "Hello Chanchito";
+    print(greet);
+
+
+greeting();
+greetingChachito();
+greeting();
+
+# IT IS NOT A GOOD PRACITCE TO USE GLOBAL VARIABLES
